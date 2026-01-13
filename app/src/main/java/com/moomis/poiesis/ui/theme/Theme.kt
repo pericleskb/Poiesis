d5750a8dcbf03e6f9e20be71eeff3cf5ee37bdbc
Periklis Karachristos <alvarinho@192.168.1.69>
2026-01-12 20:23:40 -0800

    add spacing CompositionLocal and shapes

--- app/src/main/java/com/moomis/poiesis/ui/theme/Theme.kt
+++ app/src/main/java/com/moomis/poiesis/ui/theme/Theme.kt
@@ -7,10 +7,15 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import com.moomis.poiesis.ui.theme.Spacing
 import com.moomis.poiesis.ui.theme.Typography
+import com.moomis.poiesis.ui.theme.poiesisShapes
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -252,27 +257,32 @@ val unspecified_scheme = ColorFamily(
     Color.Unspecified, Color.Unspecified, Color.Unspecified, Color.Unspecified
 )
 
+val LocalSpacing = compositionLocalOf { Spacing() }
+typealias Theme = MaterialTheme
+val Theme.spacing: Spacing
+@Composable
+@ReadOnlyComposable
+get() = LocalSpacing.current
+
 @Composable
 fun PoiesisTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
     content: @Composable() () -> Unit
 ) {
   val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-          val context = LocalContext.current
-          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      
       darkTheme -> darkScheme
       else -> lightScheme
   }
 
-  MaterialTheme(
-    colorScheme = colorScheme,
-    typography = Typography,
-    content = content
-  )
+    CompositionLocalProvider(
+        LocalSpacing provides Spacing(),
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content,
+            shapes = poiesisShapes
+        )
+    }
 }
 

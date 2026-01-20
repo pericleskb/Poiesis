e93e604f73a6c2ece3799d6c935088620c15eb1e
pericleskb <pericleskb@gmail.com>
2026-01-20 11:38:20 +0200

    add spacing CompositionLocal and shapes

--- app/src/main/java/com/moomis/poiesis/ui/home/PoemPreview.kt
+++ app/src/main/java/com/moomis/poiesis/ui/home/PoemPreview.kt
@@ -3,6 +3,8 @@ package com.moomis.poiesis.ui.home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -11,12 +13,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import com.example.compose.spacing
+import com.moomis.poiesis.BuildConfig
+import com.moomis.poiesis.R
 import com.moomis.poiesis.data.Author
 import com.moomis.poiesis.data.Poem
+import com.moomis.poiesis.ui.debugPlaceholder
 
 @Composable
 fun PoemPreview(poem: Poem, modifier: Modifier = Modifier) {
@@ -33,20 +40,25 @@ fun PoemPreview(poem: Poem, modifier: Modifier = Modifier) {
 
 @Composable
 fun AuthorWithPicture(author: Author, modifier: Modifier = Modifier) {
+
     Row(modifier = modifier.semantics(mergeDescendants = true) {}) {
         AsyncImage(
             model = author.imageURL,
             contentDescription = "Authors Image",
             contentScale = ContentScale.Crop,
-            modifier = Modifier.clip(MaterialTheme.shapes.extraLarge)
+            modifier = Modifier.size(50.dp).clip(CircleShape),
+            error = debugPlaceholder(
+                if (BuildConfig.DEBUG) R.mipmap.debug_author_placeholder else R.drawable.ic_account_circle
+            )
         )
-        Column() {
+        Column(modifier = Modifier.padding(start = MaterialTheme.spacing.small)) {
             Text(
                 text = author.name,
-                fontWeight = FontWeight.Bold
+                style = MaterialTheme.typography.headlineSmall
             )
             Text(
                 text = author.getLifeTime(),
+                style = MaterialTheme.typography.bodyMedium
             )
         }
     }
@@ -60,7 +72,7 @@ private fun AuthorWithPicturePreview() {
         dob = "Dec 10 1965",
         dod = "Jan 14 2013",
         poemsSaved = 10,
-        imageURL = "https://covers.openlibrary.org/a/olid/OL391839A-S.jpg"
+        imageURL = "https://covers.openlibrary.org/a/olid/OL391839A-S.jpg",
     )
     MaterialTheme {
         AuthorWithPicture(author = author)
--- app/src/main/java/com/moomis/poiesis/ui/theme/Type.kt
+++ app/src/main/java/com/moomis/poiesis/ui/theme/Type.kt
@@ -1,14 +1,10 @@
 package com.moomis.poiesis.ui.theme
 
-import androidx.compose.material3.R
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-
-import androidx.compose.ui.text.googlefonts.GoogleFont
-import androidx.compose.ui.text.googlefonts.Font
 
 val Typography = Typography(
     bodyLarge = TextStyle(
